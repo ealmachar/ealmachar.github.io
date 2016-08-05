e8c7de4e64c40eae296e13d28383d82e9fff4157
ealmachar <eoa775@live.com>
2016-08-05 15:19:51 -0700

    Debugging

--- src/js/app/controllers.js
+++ src/js/app/controllers.js
@@ -7,5 +7,12 @@ app.controller('mainBodyController', ['$scope', function($scope){
 		blank: 'src/html/blank.html'
 	}
 	
+	$scope.$watch(
+		'tab',
+		function(){
+			$scope.template = $scope.templates[$scope.tab];
+		}
+	)
+	
 	$scope.template = $scope.templates[$scope.tab];
 }]);
