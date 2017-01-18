13c7ae6c2d16608eada1045e2ebd014e1cdb206c
ealmachar <eoa775@live.com>
2017-01-18 15:55:39 -0800

    update

---
+++ projects/pattern.js/src/js/app/controllers.js
@@ -0,0 +1,37 @@
+app.controller('mainBodyController', ['$scope', '$window', 'patternService', function($scope, $window, patternService){
+	
+	$scope.tab = 'resume';
+	
+	$scope.pattern = patternService.pattern;
+	
+	$scope.templates = {
+		resume: 'src/html/resume.html',
+		pattern: 'src/html/pattern.html'
+	}
+	
+	$scope.show = function(page){
+		$scope.tab = page;
+		$scope.template = $scope.templates[$scope.tab];
+	}
+	
+	$scope.loadPattern = function(){
+		if($scope.tab == 'pattern'){
+			patternService.pattern.init();
+			patternService.pattern.update();
+		}
+	}
+
+	$scope.template = $scope.templates[$scope.tab];
+
+	var imageHeight = 1590;
+	var body = document.body;
+	
+
+	$window.onscroll = function() {
+		var referenceHeight = body.scrollHeight - window.innerHeight;
+		var percentage = body.scrollTop/referenceHeight;
+		var imageOffset = percentage * (imageHeight - window.innerHeight);
+		body.style.backgroundPosition = "center -" + imageOffset + "px";
+	};
+	
+}]);
